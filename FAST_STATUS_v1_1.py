72338392508bed1ea58c9fa9f13641fccf9719ac
unknown <j.maihby@gmail.com>
2020-01-05 22:02:22 +0530

    added multiple row checks

--- FAST_STATUS_v1_1.py
+++ FAST_STATUS_v1_1.py
@@ -36,6 +36,56 @@ for i in range(0,len(tran)):
     except:
         status = 'Nope'
     try:
+        status1 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[3]/td[5]').text
+        sh.cell(row = i+2, column = 6).value = status1
+    except:
+        pass
+    try:
+        status2 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[4]/td[5]').text
+        sh.cell(row = i+2, column = 7).value = status2
+    except:
+        pass
+    try:
+        status3 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[5]/td[5]').text
+        sh.cell(row = i+2, column = 8).value = status3
+    except:
+        pass
+    try:
+        status4 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[6]/td[5]').text
+        sh.cell(row = i+2, column = 9).value = status4
+    except:
+        pass
+    try:
+        status5 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[7]/td[5]').text
+        sh.cell(row = i+2, column = 10).value = status5
+    except:
+        pass
+    try:
+        status6 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[8]/td[5]').text
+        sh.cell(row = i+2, column = 11).value = status6
+    except:
+        pass
+    try:
+        status7 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[9]/td[5]').text
+        sh.cell(row = i+2, column = 12).value = status7
+    except:
+        pass
+    try:
+        status8 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[10]/td[5]').text
+        sh.cell(row = i+2, column = 13).value = status8
+    except:
+        pass
+    try:
+        status9 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[11]/td[5]').text
+        sh.cell(row = i+2, column = 14).value = status9
+    except:
+        pass
+    try:
+        status10 = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[12]/td[5]').text
+        sh.cell(row = i+2, column = 15).value = status10
+    except:
+        pass
+    try:
         amt = browser.find_element_by_xpath('//*[@id="BodyContent_gvDeposits"]/tbody/tr[2]/td[4]').text
         amt = int(float(amt.replace(',','')))
     except:
@@ -43,6 +93,7 @@ for i in range(0,len(tran)):
     browser.find_element_by_id('BodyContent_btnSearchReset').click()
     stat.append(status)
     amount.append(amt)
+    
 for i in range(2,count+2):
     try:
         sh.cell(row=i, column=4).value = stat[i-2]
